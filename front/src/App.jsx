20eaa2765ec3024bb46bc0f009c44fff9aa761eb
kwanseob <foreverdunk@naver.com>
2023-03-13 17:41:31 +0900

    Feat: Data_raw2_Export Top5 bar chart

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import NavBar from "./components/organism/NavBar";
+import DataSecond from "./components/organism/DataSecond"
 
 function App() {
   return (
     <>
       <NavBar />
+      <DataSecond/>
     </>
   );
 }
