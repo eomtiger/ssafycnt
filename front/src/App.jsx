783de7ca7ed96f2e8e968d7d0f5b4a5c2effcfd9
엄희원 <eom9300@hanmail.net>
2023-03-13 17:55:39 +0900

    Feat: dataThird

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import NavBar from "./components/organism/NavBar";
-import DataSecond from "./components/organism/DataSecond"
+import DataSecond from "./components/organism/DataSecond";
+import DataThird from "./components/organism/DataThird/index";
 
 function App() {
   return (
     <>
       <NavBar />
-      <DataSecond/>
+      <DataSecond />
+      <DataThird />
     </>
   );
 }
---
+++ front/src/components/organism/DataThird/index.jsx
@@ -0,0 +1,45 @@
+import { useMemo } from "react";
+import { faker } from "@faker-js/faker";
+import Table from "../../molecules/dataThird/Table/Table";
+
+faker.seed(100);
+faker.locale = "ko";
+
+function DataThird() {
+  const columns = useMemo(
+    () => [
+      { accessor: "nation", Header: "국가명" },
+      { accessor: "date", Header: "조회기준" },
+      { accessor: "amount", Header: "수입금액($)" },
+      { accessor: "amountPortion", Header: "점유율(금액)" },
+      { accessor: "weight", Header: "수입중량(kg)" },
+      { accessor: "weightPortion", Header: "점유율(중량)" },
+      { accessor: "hsCode", Header: "품목코드HS코드" },
+    ],
+    []
+  );
+
+  const data = useMemo(
+    () =>
+      Array(100)
+        .fill()
+        .map(() => ({
+          nation: faker.address.country(),
+          date: "2022.3 - 2023.3",
+          amount: faker.commerce.price(0, 100000000, 0, "$"),
+          amountPortion: "11%",
+          weight: faker.datatype.number({ min: 100000 }),
+          weightPortion: "11%",
+          hsCode: "전체품목",
+        })),
+    []
+  );
+
+  return (
+    <div>
+      <Table columns={columns} data={data} />
+    </div>
+  );
+}
+
+export default DataThird;
