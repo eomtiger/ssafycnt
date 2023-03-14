fdbe74aa487646f2135f3d604b0662e061c61e0a
김이삭 <40036437+Isak-toast@users.noreply.github.com>
2023-03-14 17:52:26 +0900

    Add: Back End service 추가 인증 구현해야 하는데 못했다..ㅠ

--- BackEnd/ssafycnt-user-service/src/main/java/com/ssafy/ssafycntuserservice/controller/UserController.java
+++ BackEnd/ssafycnt-user-service/src/main/java/com/ssafy/ssafycntuserservice/controller/UserController.java
@@ -4,10 +4,14 @@ import com.ssafy.ssafycntuserservice.dto.UserDto;
 import com.ssafy.ssafycntuserservice.service.UserService;
 import com.ssafy.ssafycntuserservice.vo.Greeting;
 import com.ssafy.ssafycntuserservice.vo.RequestUser;
+import com.ssafy.ssafycntuserservice.vo.ResponseUser;
+import org.apache.tomcat.util.http.ResponseUtil;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -35,13 +39,29 @@ public class UserController {
         return greeting.getMessage();
     }
 
+//    @PostMapping("/users")
+//    public String createUser(@RequestBody RequestUser user){
+//        ModelMapper mapper = new ModelMapper();
+//        mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+//
+//        UserDto userDto = mapper.map(user, UserDto.class);
+//        userService.createUser(userDto);
+//        return "Create user method is called";
+//    }
+    /*
+    ResponseEntity 객체에 ResponseUser 개체를 보내줌으로 Modelmappp을 통해서
+    UserDto에서 ResponseUser 개체의 mapping되는 컬럼만을 받는다.
+     */
     @PostMapping("/users")
-    public String createUser(@RequestBody RequestUser user){
+    public ResponseEntity<ResponseUser> createUser(@RequestBody RequestUser user){
         ModelMapper mapper = new ModelMapper();
         mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
 
         UserDto userDto = mapper.map(user, UserDto.class);
         userService.createUser(userDto);
-        return "Create user method is called";
+
+        ResponseUser responseUser = mapper.map(userDto, ResponseUser.class);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseUser);
     }
 }
