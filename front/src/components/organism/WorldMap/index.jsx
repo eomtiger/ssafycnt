3bba6521bfe89a8cbd3bd228ec7fa76daf7045f8
엄희원 <eomtiger@gmail.com>
2023-03-15 13:51:57 +0900

    Feat: worldmap color test

--- front/src/components/organism/WorldMap/index.jsx
+++ front/src/components/organism/WorldMap/index.jsx
@@ -3,19 +3,28 @@ import Chart from "react-google-charts";
 
 function WorldMap() {
   const data = [
-    ["Country", "???", "수출입"],
-    ["Germany", 200, 100],
-    ["United States", 300, 200],
-    ["Brazil", 400, 300],
-    ["Canada", 500, 400],
-    ["France", 600, 500],
-    ["RU", 700, 600],
-    ["China", 500, 799],
-    ["South Korea", 1000, 1000],
+    ["Country", "수출입", "수출"],
+    ["Germany", 2, 100],
+    ["United States", 3, 100],
+    ["Brazil", 2, 100],
+    ["Canada", 3, 100],
+    ["France", 1, 100],
+    ["RU", 1, 100],
+    ["China", 2, 100],
+    ["South Korea", 2, 100],
   ];
+  const options = {
+    colorAxis: { colors: ["red", "yellow", "blue"] },
+  };
   return (
     <>
-      <Chart chartType="GeoChart" width="100%" height="400px" data={data} />
+      <Chart
+        chartType="GeoChart"
+        width="100%"
+        height="600px"
+        data={data}
+        options={options}
+      />
     </>
   );
 }
