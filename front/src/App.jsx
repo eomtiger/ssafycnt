53934b223e23cd81c07f0c50424d819b214ef381
엄희원 <eom9300@hanmail.net>
2023-03-15 13:07:59 +0900

    Feat: worldmap test

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -2,11 +2,12 @@ import "./App.css";
 import NavBar from "./components/organism/NavBar";
 import DataSecond from "./components/organism/DataSecond";
 import DataThird from "./components/organism/DataThird/index";
-
+import WorldMap from "./components/organism/WorldMap";
 function App() {
   return (
     <>
       <NavBar />
+      <WorldMap />
       <DataSecond />
       <DataThird />
     </>
