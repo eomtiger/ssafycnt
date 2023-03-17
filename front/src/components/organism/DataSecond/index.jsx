7a4fa84427217ffed55b713627ad1ca1a7736936
kwanseob <foreverdunk@naver.com>
2023-03-17 17:33:45 +0900

    Feat: Datarow2_LineChart Addition

--- front/src/components/organism/DataSecond/index.jsx
+++ front/src/components/organism/DataSecond/index.jsx
@@ -1,12 +1,14 @@
-import BarChart from './../../molecules/dataSecond/ExportTop5/index'
+import BarChartExport from './../../molecules/dataSecond/ExportTop5/index'
 import BarChartImport from './../../molecules/dataSecond/ImportTop5/index'
+import LineChartTrend from './../../molecules/dataSecond/LineGraph/index'
 
 function DataSecond() {
   return (
-      <div className="flex justify-between space-x-5 top-0 border-8 border--400">
-      <BarChart />
+    <div className="flex justify-between space-x-5 top-0 border-8 border--400">
+      <LineChartTrend />
+      <BarChartExport />
       <BarChartImport />
-      </div>
+    </div>
   );
 }
 
