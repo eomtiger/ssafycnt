0a6a8e51e7ccc4546557c830600d1538540b09a4
엄희원 <eom9300@hanmail.net>
2023-03-21 14:04:41 +0900

    Feat: world map clickEvent test

--- front/src/components/organism/WorldMap/index.jsx
+++ front/src/components/organism/WorldMap/index.jsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Chart from "react-google-charts";
 
 function WorldMap() {
+  const [a, setA] = useState(-1);
   const data = [
     ["Country", "수출입", "수출"],
     ["Germany", 2, 100],
@@ -10,20 +11,44 @@ function WorldMap() {
     ["Canada", 3, 100],
     ["France", 1, 100],
     ["RU", 1, 100],
-    ["China", 2, 100],
+    ["CN", 2, 100],
     ["South Korea", 2, 100],
+    ["Japan", 1, 100],
   ];
   const options = {
+    // backgroundColor: "",
     colorAxis: { colors: ["red", "yellow", "blue"] },
+    // datalessRegionColor: "black",
+    // forceIFrame: 20,
+    geochartVersion: 11,
+    height: 550,
+    legend: "none",
+    // tooltip: { textStyle: { color: "black" }, showColorCode: true },
+    tooltip: { trigger: "focus" },
   };
+
+  const chartEvents = [
+    {
+      eventName: "select",
+
+      callback({ chartWrapper }) {
+        const selectedId = chartWrapper.getChart().getSelection();
+        if (selectedId.length) {
+          console.log("selected Id", selectedId);
+          setA(selectedId[0]["row"]);
+          console.log(data[a + 1][0]);
+        }
+      },
+    },
+  ];
   return (
     <>
+      <div className="mb-5">수출 수입</div>
       <Chart
         chartType="GeoChart"
-        // width="80%"
-        // height="50px"
         data={data}
         options={options}
+        chartEvents={chartEvents}
       />
     </>
   );
