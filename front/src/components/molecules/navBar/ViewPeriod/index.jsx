555e4483054ea1e091f8c56cf4168cd27ffc16fe
powerjjh95 <powerjjh95@gmail.com>
2023-03-21 17:25:27 +0900

    Fix : NavBar Out of Memory Error

--- front/src/components/molecules/navBar/ViewPeriod/index.jsx
+++ front/src/components/molecules/navBar/ViewPeriod/index.jsx
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from "react";
-import ReactDOM from "react-dom";
+import React, { useState } from "react";
 import Modal from "react-modal";
 import Select from "react-select";
 
@@ -16,39 +15,63 @@ import Select from "react-select";
 
 // 오늘 날짜
 const today = new Date();
-// 오늘 기준 년
+// // 오늘 기준 년
 const todayYear = today.getFullYear();
-// 오늘 기준 일
-const todayMonth = today.getMonth() + 1;
-// 오늘 년 기준 10년전
-const yearList = [];
-for (let i = todayYear; i >= todayYear - 10; i--) {
-  {
-    yearList.push({ value: i, label: i });
-  }
-}
-// console.log(yearList[0].value);
+// // 오늘 기준 일
+const todayMonth = today.getMonth();
+// // 오늘 년 기준 10년전
+const yearList = [
+  { value: 2023, label: 2023 },
+  { value: 2022, label: 2022 },
+  { value: 2021, label: 2021 },
+  { value: 2020, label: 2020 },
+  { value: 2019, label: 2019 },
+];
+// const yearList = [];
+// for (let i = todayYear; i >= todayYear - 10; i--) {
+//   {
+//     yearList.push({ value: i, label: i });
+//   }
+// }
+// // console.log(yearList[0].value);
 
 // 올해 년도 기준, 월 표시
-const todayYearMonthList = [];
-for (let i = 1; i <= todayMonth; i--) {
-  {
-    todayYearMonthList.push({ value: i, label: i });
-  }
-}
+const todayYearMonthList = [
+  { value: 1, label: 1 },
+  { value: 2, label: 2 },
+  { value: 3, label: 3 },
+];
+// for (let i = 1; i <= todayMonth; i--) {
+//   {
+//     todayYearMonthList.push({ value: i, label: i });
+//   }
+// }
 
 // 1월 ~ 12월
-const monthList = [];
-for (let i = 1; i <= 12; i++) {
-  {
-    monthList.push({ value: i, label: i });
-  }
-}
+const monthList = [
+  { value: 1, label: 1 },
+  { value: 2, label: 2 },
+  { value: 3, label: 3 },
+  { value: 4, label: 4 },
+  { value: 5, label: 5 },
+  { value: 6, label: 6 },
+  { value: 7, label: 7 },
+  { value: 8, label: 8 },
+  { value: 9, label: 9 },
+  { value: 10, label: 10 },
+  { value: 11, label: 11 },
+  { value: 12, label: 12 },
+];
+
+// for (let i = 1; i <= 12; i++) {
+//   {
+//     monthList.push({ value: i, label: i });
+//   }
+// }
 // console.log(monthList);
 
 // 함수 시작
 function ViewPeriod() {
-  let subtitle;
   const [modalIsOpen, setIsOpen] = React.useState(false);
 
   // Modal을 Open하는 함수
@@ -58,7 +81,6 @@ function ViewPeriod() {
 
   const afterOpenModal = () => {
     // references are now sync'd and can be accessed.
-    // subtitle.style.color = "#f00";
   };
 
   // Modal을 Close하는 함수
@@ -66,7 +88,7 @@ function ViewPeriod() {
     setIsOpen(false);
   };
 
-  //
+  // 연도 선택 함수
   const [selectedYear, setSelectedYear] = useState();
   const yearHandler = (event) => {
     setSelectedYear(event.value);
@@ -88,11 +110,12 @@ function ViewPeriod() {
         // style={customStyles}
         contentLabel="Example Modal"
       >
-        <h2 ref={(_subtitle) => (subtitle = _subtitle)}>Hello</h2>
+        {/* <h2 ref={(_subtitle) => (subtitle = _subtitle)}>Hello</h2> */}
         <div>
           <h2>시작 년/월</h2>
           <h3>시작 년도</h3>
           <Select options={yearList} onChange={yearHandler} />
+          {/* <Select options={yearList} /> */}
           <h3>시작 월</h3>
           {yearData.year === todayYear ? (
             <Select options={todayYearMonthList} />
@@ -113,3 +136,11 @@ function ViewPeriod() {
 }
 
 export default ViewPeriod;
+
+// import React from "react";
+
+// function ViewPeriod() {
+//   return <></>;
+// }
+
+// export default ViewPeriod;
