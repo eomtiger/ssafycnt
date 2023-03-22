6ce403d908ceacb6694abf663ca020ff0b4025b9
robotdiary <molihua321.dp@gmail.com>
2023-03-22 16:53:00 +0900

    Feat: NewsService upload

---
+++ back/ssafycnt-news-service/src/test/java/com/ssafy/ssafycntnewsservice/SsafycntNewsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.ssafycntnewsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsafycntNewsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
