ad66b88f9dd2ea41f647170f8412945b20f58731
robotdiary <molihua321.dp@gmail.com>
2023-03-22 17:32:47 +0900

    실험

--- back/ssafycnt-news-service/src/main/java/com/ssafy/ssafycntnewsservice/controller/NewsController.java
+++ back/ssafycnt-news-service/src/main/java/com/ssafy/ssafycntnewsservice/controller/NewsController.java
@@ -5,6 +5,7 @@ import com.ssafy.ssafycntnewsservice.service.MiningService;
 import com.ssafy.ssafycntnewsservice.service.NewsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -12,13 +13,20 @@ import java.util.Map;
 
 @RestController // @Controller와 @ResponseBody가 합쳐진 어노테이션으로 주 용도는 Json 형태로 객체 데이터를 반환하는 것
 @RequiredArgsConstructor //Lombok의 기능으로 final이 붙거나 @NotNull 이 붙은 필드의 생성자를 자동 생성
+@RequestMapping("/")
 public class NewsController {
     private final NewsService newsService;
     private final MiningService miningService;
     List<NewsDto> newsdata;
+    @GetMapping("/health_check")
+    public String HealthCheck() {
+        return "Hello world";
+    }
+
     @GetMapping("/news-api")
     public List<NewsDto> NewsController() {
         newsdata = newsService.getNewsData();
+
         return newsdata;
     }
     @GetMapping("/news-api/mining")
