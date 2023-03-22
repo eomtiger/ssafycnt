be7d408436d26cad4338fca2d5b171f62336b0e3
김이삭 <40036437+Isak-toast@users.noreply.github.com>
2023-03-22 17:47:10 +0900

    feat: user service... dummy server 제작 중

---
+++ back/ssafycnt-config-service/settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "ssafycnt-config-service"
---
+++ back/ssafycnt-config-service/src/test/java/com/ssafy/ssafycntconfigservice/SsafycntConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.ssafycntconfigservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsafycntConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
