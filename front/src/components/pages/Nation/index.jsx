487b1d4016f6ec35281864615362445cc87fd213
엄희원 <eom9300@hanmail.net>
2023-03-23 16:02:08 +0900

    Feat: data props

--- front/src/components/pages/Nation/index.jsx
+++ front/src/components/pages/Nation/index.jsx
@@ -1,18 +1,32 @@
-import React from "react";
-
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import NavBar from "../../organism/NavBar";
-
 import DataFirst from "../../organism/DataFirst";
 import DataSecond from "../../organism/DataSecond";
 import DataThird from "../../organism/DataThird/index";
 import WorldMap from "../../organism/WorldMap";
 
 function Nation() {
+  const params = useParams();
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://98320413-724a-44ba-a0b5-9b226001b6d6.mock.pstmn.io/api/nation/" +
+          params.nationName +
+          "/" +
+          params.duration
+      )
+      .then((response) => setData(response.data));
+  }, [params]);
+
   return (
     <>
       <NavBar />
-
-      <WorldMap />
+      <WorldMap data1={data} />
       <DataFirst />
       <DataSecond />
       <DataThird />
