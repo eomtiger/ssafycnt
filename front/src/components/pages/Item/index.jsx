50bb446e365af28f3e92dd4baf9a297a15993cdd
엄희원 <eom9300@hanmail.net>
2023-03-24 12:57:43 +0900

    Feat: 국가 데이터 3행 UN국기 추가

--- front/src/components/pages/Item/index.jsx
+++ front/src/components/pages/Item/index.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-
+import DataThirdI from "../../organism/DataThirdI";
 function Item() {
   return (
     <>
       <div>품목 페이지입니다</div>
+      <DataThirdI />
     </>
   );
 }
