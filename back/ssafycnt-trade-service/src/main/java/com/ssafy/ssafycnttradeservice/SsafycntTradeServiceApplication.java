2ec3317c8109ee3e1839e469fe92a75d33e02716
백승범 <tmdqjaqhdl3@naver.com>
2023-03-27 17:38:43 +0900

    Develop : trade-service 개발

--- back/ssafycnt-trade-service/src/main/java/com/ssafy/ssafycnttradeservice/SsafycntTradeServiceApplication.java
+++ back/ssafycnt-trade-service/src/main/java/com/ssafy/ssafycnttradeservice/SsafycntTradeServiceApplication.java
@@ -2,14 +2,18 @@ package com.ssafy.ssafycnttradeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-@EnableEurekaClient
+//@EnableDiscoveryClient
 public class SsafycntTradeServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SsafycntTradeServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SsafycntTradeServiceApplication.class, args);
+    }
 
+//    @Bean
+//    public BCryptPasswordEncoder passwordEncoder(){
+//        return new BCryptPasswordEncoder();
+//    }
 }
