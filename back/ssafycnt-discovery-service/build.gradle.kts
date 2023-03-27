affe563359c823df5f5dc3ab079d1c7322b91c02
김이삭 <isakggong@gmail.com>
2023-03-27 09:22:28 +0900

    fix: new service pulling

--- back/ssafycnt-discovery-service/build.gradle.kts
+++ back/ssafycnt-discovery-service/build.gradle.kts
@@ -17,6 +17,15 @@ extra["springCloudVersion"] = "2021.0.6"
 dependencies {
 	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-server")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	compileOnly("org.projectlombok:lombok")
+	annotationProcessor("org.projectlombok:lombok")
+	implementation ("org.springframework.cloud:spring-cloud-starter-config")
+	implementation ("org.springframework.cloud:spring-cloud-starter-bootstrap")
+	implementation ("org.springframework.cloud:spring-cloud-starter-bus-amqp")
+	implementation ("org.springframework.boot:spring-boot-starter-actuator")
+//    implementation 'org.springframework.cloud:spring-cloud-starter-sleuth'
+//    implementation 'org.springframework.cloud:spring-cloud-starter-zipkin:2.2.3.RELEASE'
+	// https://mvnrepository.com/artifact/io.micrometer/micrometer-registry-prometheus
 }
 
 dependencyManagement {
--- back/ssafycnt-user-service/build.gradle.kts
+++ back/ssafycnt-user-service/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     java
     id("org.springframework.boot") version "2.7.9"
     id("io.spring.dependency-management") version "1.0.15.RELEASE"
+    id("org.sonarqube") version "3.0"
 }
 
 group = "com.ssafy"
@@ -24,19 +25,34 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
     implementation("org.springframework.boot:spring-boot-starter-validation")
-    compileOnly("org.projectlombok:lombok")
-    developmentOnly("org.springframework.boot:spring-boot-devtools")
-    annotationProcessor("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-    // https://mvnrepository.com/artifact/com.h2database/h2
-    implementation("com.h2database:h2:1.3.176")
-    // https://mvnrepository.com/artifact/org.springframework.boot/spring-boot-starter-data-jpa
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa:2.7.9")
-    // https://mvnrepository.com/artifact/org.modelmapper/modelmapper
-    implementation("org.modelmapper:modelmapper:2.3.8")
-    // https://mvnrepository.com/artifact/org.springframework.boot/spring-boot-starter-security
-    implementation("org.springframework.boot:spring-boot-starter-security:2.7.9")
+    implementation ("org.springframework.cloud:spring-cloud-starter-config")
+    implementation ("org.springframework.cloud:spring-cloud-starter-bootstrap")
+    implementation ("org.springframework.cloud:spring-cloud-starter-openfeign")
+    implementation ("org.springframework.cloud:spring-cloud-starter-bus-amqp")
+    implementation ("org.springframework.boot:spring-boot-starter-actuator")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+// https://mvnrepository.com/artifact/org.springframework.boot/spring-boot-starter-security
+    implementation("org.springframework.boot:spring-boot-starter-security")
 
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
+//    implementation ("org.springframework.cloud:spring-cloud-starter-circuitbreaker-resilience4j")
+    implementation ("org.springframework.boot:spring-boot-starter-data-jpa")
+//    implementation ("org.springframework.boot:spring-boot-starter-data-mongodb")
+//    implementation ("org.springframework.boot:spring-boot-starter-data-redis")
+//    implementation ("org.springframework.boot:spring-boot-starter-cache")
+
+    // https://mvnrepository.com/artifact/com.mysql/mysql-connector-j
+    implementation("com.mysql:mysql-connector-j:8.0.32")
+    // https://mvnrepository.com/artifact/org.springframework.boot/spring-boot-starter-security
+    implementation("org.modelmapper:modelmapper:3.1.1")
+
+    implementation("org.springdoc:springdoc-openapi-ui:1.6.15")
+    // https://mvnrepository.com/artifact/io.springfox/springfox-boot-starter
+    implementation("io.springfox:springfox-boot-starter:3.0.0")
+    // https://mvnrepository.com/artifact/com.h2database/h2
+    implementation("com.h2database:h2:2.1.214")
 
 }
 
