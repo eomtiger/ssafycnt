f8572f03512c100460b69d21af7f7317db9eb625
김이삭 <isakggong@gmail.com>
2023-03-27 09:22:28 +0900

    fix: new service pulling

--- back/ssafycnt-apigateway-service/build.gradle.kts
+++ back/ssafycnt-apigateway-service/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
 	java
 	id("org.springframework.boot") version "2.7.9"
 	id("io.spring.dependency-management") version "1.0.15.RELEASE"
+    id("org.sonarqube") version "3.0"
 }
 
 group = "com.ssafy"
@@ -26,7 +27,17 @@ dependencies {
 	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
-
+	implementation ("org.springframework.cloud:spring-cloud-starter-config")
+	implementation ("org.springframework.cloud:spring-cloud-starter-bootstrap")
+	implementation ("org.springframework.cloud:spring-cloud-starter-bus-amqp")
+//    implementation 'org.springframework.cloud:spring-cloud-starter-sleuth'
+//    implementation 'org.springframework.cloud:spring-cloud-starter-zipkin:2.2.3.RELEASE'
+	// https://mvnrepository.com/artifact/io.micrometer/micrometer-registry-prometheus
+	implementation ("org.springframework.boot:spring-boot-starter-actuator")
+	// https://mvnrepository.com/artifact/org.springdoc/springdoc-openapi-ui
+	implementation("org.springdoc:springdoc-openapi-ui:1.6.14")
+	// https://mvnrepository.com/artifact/io.springfox/springfox-boot-starter
+	implementation("io.springfox:springfox-boot-starter:3.0.0")
 }
 
 dependencyManagement {
