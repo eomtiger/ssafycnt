2648882834659e49f665e335c663f8f722ccbbb2
엄희원 <eom9300@hanmail.net>
2023-03-28 15:58:19 +0900

    Feat: 세계지도 데이터 통신

--- front/src/components/organism/WorldMap/index.jsx
+++ front/src/components/organism/WorldMap/index.jsx
@@ -1,42 +1,81 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
+import axios from "axios";
 import Chart from "react-google-charts";
 
-function WorldMap({ data1 }) {
+function WorldMap() {
   const params = useParams();
+  const duration = params.duration;
   const [a, setA] = useState(params.nationCode);
   const navigate = useNavigate();
 
-  const data = [
-    ["Country", "수출입", "수출"],
-    ["Germany", 2, 100],
-    ["US", 3, 100],
-    ["Brazil", 2, 100],
-    ["Canada", 3, 100],
-    ["France", 1, 100],
-    ["RU", 1, 100],
-    ["CN", 2, 100],
-    ["South Korea", 2, 100],
-    ["Japan", 1, 100],
-  ];
+  // const data1 = [
+  //   ["Country", "balnce"],
+  //   ["DE", -100],
+  //   ["US", -75],
+  //   ["BR", -50],
+  //   ["CA", -25],
+  //   ["FR", 0],
+  //   ["RU", 25],
+  //   ["CN", 50],
+  //   ["South Korea", 75],
+  //   ["JP", 100],
+  // ];
   const setAHandler = (e) => {
     setA(data[e][0]);
   };
+
+  const [data, setData] = useState([]);
+
+  const dataHandler = (data) => {
+    const temp = [["Country", "balance"]];
+    for (let i in data) {
+      const item = [];
+
+      item.push(i);
+      item.push(data[i]["balpaymentsLr"]);
+      temp.push(item);
+    }
+
+    setData(temp);
+  };
+
+  //여기서 axios
+  useEffect(() => {
+    axios
+      .get(
+        "https://98320413-724a-44ba-a0b5-9b226001b6d6.mock.pstmn.io/api/trade/country/map?" +
+          "startDate=" +
+          params.duration.substring(0, 6) +
+          "&" +
+          "endDate=" +
+          params.duration.substring(7, 12)
+      )
+      .then((response) => {
+        dataHandler(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [duration]);
 
   useEffect(() => {
     navigate("/nation/" + a + "/" + params.duration);
   }, [a]);
 
   const options = {
-    // backgroundColor: "",
-    colorAxis: { colors: ["FCA5A5", "FDE047", "93C5FD"] },
-    // datalessRegionColor: "black",
+    backgroundColor: "93C5FD",
+    colorAxis: {
+      colors: ["008000"],
+    },
+    datalessRegionColor: "white",
     // forceIFrame: 20,
     geochartVersion: 11,
     height: 550,
-    legend: "none",
+    // legend: "none",
     // tooltip: { textStyle: { color: "black" }, showColorCode: true },
     tooltip: { trigger: "focus" },
+    // sizeAxis: { minValue: 0, maxSize: 20 },
   };
 
   const chartEvents = [
@@ -55,22 +94,26 @@ function WorldMap({ data1 }) {
 
   return (
     <>
-      <div className="flex mb-5 flex-start ml-10 mt-5">
-        <div className="w-5 h-5 rounded-full bg-red-300 mr-3"></div>
+      {/* <div className="flex mb-5 flex-start ml-10 mt-5 "> */}
+      {/* <div className="w-5 h-5 rounded-full bg-red-900 mr-3"></div>
         <div className="mr-5">수출</div>
-        <div className="w-5 h-5 rounded-full bg-blue-300 mr-3"></div>
-        <div className="mr-5">수입</div>
-        <div className="w-5 h-5 rounded-full bg-yellow-300 mr-3"></div>
-        <div className="mr-5">수출입</div>
-        <div className="ml-20">{params.nationName}</div>
-        <div className="ml-20">balance :{data1.balpayments}</div>
+        <div className="w-5 h-5 rounded-full bg-blue-900 mr-3"></div>
+        <div className="mr-5">수입</div> */}
+      {/* <div className="w-5 h-5 rounded-full bg-yellow-300 mr-3"></div> */}
+      {/* <div className="mr-5">수출입</div> */}
+      {/* <div className="ml-20">{params.nationName}</div> */}
+      {/* </div> */}
+      <div className="mt-5 static">
+        <Chart
+          chartType="GeoChart"
+          data={data}
+          options={options}
+          chartEvents={chartEvents}
+        />
+        <div className="absolute bottom-0 left-3 mt-5">
+          <p className="font-mun">무역 수지</p>
+        </div>
       </div>
-      <Chart
-        chartType="GeoChart"
-        data={data}
-        options={options}
-        chartEvents={chartEvents}
-      />
     </>
   );
 }
