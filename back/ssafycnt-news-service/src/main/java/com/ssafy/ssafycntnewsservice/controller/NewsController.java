e52734981549520061f54f98dd770e26a99c07f5
김진희 <molihua321.dp@gmail.com>
2023-03-29 09:17:55 +0900

    Test: 자동배포확인

--- back/ssafycnt-news-service/src/main/java/com/ssafy/ssafycntnewsservice/controller/NewsController.java
+++ back/ssafycnt-news-service/src/main/java/com/ssafy/ssafycntnewsservice/controller/NewsController.java
@@ -12,9 +12,9 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 import java.util.Map;
 
-@RestController // @Controller와 @ResponseBody가 합쳐진 어노테이션으로 주 용도는 Json 형태로 객체 데이터를 반환하는 것
+@RestController
 @RequestMapping("/api")
-@RequiredArgsConstructor //Lombok의 기능으로 final이 붙거나 @NotNull 이 붙은 필드의 생성자를 자동 생성
+@RequiredArgsConstructor
 public class NewsController {
     private final NewsService newsService;
     private final MiningService miningService;
@@ -22,9 +22,14 @@ public class NewsController {
     public List<NewsDto> NewsController(@RequestParam String country, String item, String startDate, String endDate) {
         return newsService.getNewsData(country, item, startDate, endDate);
     }
+//    @GetMapping("/news/mining")
+//    public Map<String, List<NewsDto>> TextMining(@RequestParam String country, String item, String startDate, String endDate) {
+//        List<NewsDto> newsdata = newsService.getNewsData(country, item, startDate, endDate);
+//        return miningService.getMiningData(newsdata);
+//    }
     @GetMapping("/news/mining")
-    public Map<String, List<NewsDto>> TextMining(@RequestParam String country, String item, String startDate, String endDate) {
+    public String TextMining(@RequestParam String country, String item, String startDate, String endDate) {
         List<NewsDto> newsdata = newsService.getNewsData(country, item, startDate, endDate);
-        return miningService.getMiningData(newsdata);
+        return "장준호 바보";
     }
 }
