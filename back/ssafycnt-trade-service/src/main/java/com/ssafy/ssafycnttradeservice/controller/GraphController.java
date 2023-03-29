1ce36eba7bfa20499fd49fea9ce78a9d9324113b
백승범 <tmdqjaqhdl3@naver.com>
2023-03-29 10:42:51 +0900

    0329

--- back/ssafycnt-trade-service/src/main/java/com/ssafy/ssafycnttradeservice/controller/GraphController.java
+++ back/ssafycnt-trade-service/src/main/java/com/ssafy/ssafycnttradeservice/controller/GraphController.java
@@ -1,9 +1,10 @@
 package com.ssafy.ssafycnttradeservice.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ssafy.ssafycnttradeservice.domain.Graph;
-import com.ssafy.ssafycnttradeservice.dto.ByCountryRequestDTO;
+import com.ssafy.ssafycnttradeservice.dto.request.Row1RequestDTO;
+import com.ssafy.ssafycnttradeservice.dto.response.Row1ResponseDTO;
+import com.ssafy.ssafycnttradeservice.service.GraphService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -12,11 +13,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
-import javax.persistence.TypedQuery;
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -24,21 +20,11 @@ import java.util.List;
 @Slf4j
 @RequestMapping("/api")
 public class GraphController {
-    @PersistenceContext
-    private EntityManager em;
+    private final GraphService graphService;
 
     @PostMapping("/trade/country")
-    public ResponseEntity<?> searchDataByCountry(@RequestBody ByCountryRequestDTO dto) throws JsonProcessingException {
-          String tableName = dto.getStatCd()+"_trading";
-          String sql = "select * from " + tableName + " where year between " +
-                  dto.getStartDate() + " and " + dto.getEndDate();
-        List<Graph> resultDtos = (List<Graph>) em.createNativeQuery(sql).getResultList();
-//        ObjectMapper mapper = new ObjectMapper();
-//        List<String> jsonString = new ArrayList<>();
-//        for(Graph g : resultDtos) {
-//            jsonString.add(mapper.writeValueAsString(g));
-//        }
-//        List<Graph> resultDtos= nativeQuery.getResultList();
-        return ResponseEntity.ok(resultDtos);
+    public ResponseEntity<?> searchDataByCountry(@RequestBody Row1RequestDTO dto) throws JsonProcessingException {
+        List<Graph> list = graphService.findDataByCountry(dto.getStatCd(),dto.getStartDate(),dto.getEndDate());
+        return ResponseEntity.ok(new Row1ResponseDTO(list,dto.getStatCd(),dto.getStartDate(),dto.getEndDate()));
     }
 }
--- back/ssafycnt-trade-service/src/main/java/com/ssafy/ssafycnttradeservice/dto/request/Row1RequestDTO.java
+++ back/ssafycnt-trade-service/src/main/java/com/ssafy/ssafycnttradeservice/dto/request/Row1RequestDTO.java
@@ -1,4 +1,4 @@
-package com.ssafy.ssafycnttradeservice.dto;
+package com.ssafy.ssafycnttradeservice.dto.request;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class ByCountryRequestDTO {
+public class Row1RequestDTO {
     private String statCd;
     private String startDate;
     private String endDate;
