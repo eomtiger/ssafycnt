433e4900fa1eec14955c3ea28386c6619d6857b0
백승범 <tmdqjaqhdl3@naver.com>
2023-03-31 14:27:59 +0900

    Feat : 품목 API 0,1,3 개발

--- back/ssafycnt-trade-service/src/main/java/com/ssafy/ssafycnttradeservice/controller/GraphController.java
+++ back/ssafycnt-trade-service/src/main/java/com/ssafy/ssafycnttradeservice/controller/GraphController.java
@@ -2,10 +2,12 @@ package com.ssafy.ssafycnttradeservice.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.ssafy.ssafycnttradeservice.domain.Graph;
-import com.ssafy.ssafycnttradeservice.dto.response.Row1ResponseDTO;
-import com.ssafy.ssafycnttradeservice.dto.response.Row2ResponseDTO;
-import com.ssafy.ssafycnttradeservice.dto.response.Row3ResponseDTO;
-import com.ssafy.ssafycnttradeservice.service.GraphService;
+import com.ssafy.ssafycnttradeservice.dto.response.item.ItemRow1ResponseDTO;
+import com.ssafy.ssafycnttradeservice.dto.response.stat.StatRow1ResponseDTO;
+import com.ssafy.ssafycnttradeservice.dto.response.stat.StatRow2ResponseDTO;
+import com.ssafy.ssafycnttradeservice.dto.response.stat.StatRow3ResponseDTO;
+import com.ssafy.ssafycnttradeservice.service.ItemGraphService;
+import com.ssafy.ssafycnttradeservice.service.StatGraphService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -20,37 +22,69 @@ import java.util.Map;
 @RequestMapping("/api")
 @CrossOrigin(origins = {"http://ssafycnt.site", "http://localhost:5173" })
 public class GraphController {
-    private final GraphService graphService;
+    private final StatGraphService statGraphService;
+    private final ItemGraphService itemGraphService;
 
     @GetMapping("/trade/onerow")
-    public ResponseEntity<?> searchOneRow(@RequestParam String statCd, @RequestParam String startDate,
+    public ResponseEntity<?> searchOneRowPerStat(@RequestParam String statCd, @RequestParam String startDate,
                                                  @RequestParam String endDate) throws JsonProcessingException {
-        List<Graph> list = graphService.findOneRow(statCd,startDate,endDate);
+        List<Graph> list = statGraphService.findOneRowPerStat(statCd,startDate,endDate);
         startDate = Change(startDate);
         endDate = Change(endDate);
-        return ResponseEntity.ok(new Row1ResponseDTO(list,statCd,startDate,endDate));
+        return ResponseEntity.ok(new StatRow1ResponseDTO(list,statCd,startDate,endDate));
     }
 
     @GetMapping("/trade/tworow")
-    public ResponseEntity<?> searchTwoRow(@RequestParam String statCd, @RequestParam String startDate,
+    public ResponseEntity<?> searchTwoRowPerStat(@RequestParam String statCd, @RequestParam String startDate,
                                                  @RequestParam String endDate) throws JsonProcessingException {
-        List<Map<String, Object>> list = graphService.findTwoRow(statCd,startDate,endDate);
+        List<Map<String, Object>> list = statGraphService.findTwoRowPerStat(statCd,startDate,endDate);
         startDate = Change(startDate);
         endDate = Change(endDate);
-        return ResponseEntity.ok(new Row2ResponseDTO(list,statCd,startDate,endDate));
+        return ResponseEntity.ok(new StatRow2ResponseDTO(list,statCd,startDate,endDate));
     }
 
     @GetMapping("/trade/threerow")
-    public ResponseEntity<?> searchThreeRow(@RequestParam String startDate, @RequestParam String endDate) throws JsonProcessingException {
-        List<Map<String, Object>> list = graphService.findThreeRow(startDate,endDate);
+    public ResponseEntity<?> searchThreeRowPerStat(@RequestParam String startDate, @RequestParam String endDate) throws JsonProcessingException {
+        List<Map<String, Object>> list = statGraphService.findThreeRowPerStat(startDate,endDate);
         startDate = Change(startDate);
         endDate = Change(endDate);
-        return ResponseEntity.ok(new Row3ResponseDTO(list,startDate,endDate));
+        return ResponseEntity.ok(new StatRow3ResponseDTO(list,startDate,endDate));
     }
 
-    @GetMapping("/trade/fourrow")
-    public ResponseEntity<?> searchFourRow(@RequestParam String startDate, @RequestParam String endDate) throws JsonProcessingException {
-        Map<String, Object> list = graphService.findFourRow(startDate,endDate);
+    @GetMapping("/trade/zerorow")
+    public ResponseEntity<?> searchZeroRowPerStat(@RequestParam String startDate, @RequestParam String endDate) throws JsonProcessingException {
+        Map<String, Object> list = statGraphService.findZeroRowPerStat(startDate,endDate);
+        startDate = Change(startDate);
+        endDate = Change(endDate);
+        return ResponseEntity.ok(list);
+    }
+
+    @GetMapping("/trade/item/onerow")
+    public ResponseEntity<?> searchOneRowPerItem(@RequestParam String item,
+                                                 @RequestParam String startDate,
+                                                 @RequestParam String endDate) throws JsonProcessingException {
+        Map<String, Object> list = itemGraphService.findOneRowPerItem(item,startDate,endDate);
+        startDate = Change(startDate);
+        endDate = Change(endDate);
+        return ResponseEntity.ok(list);
+    }
+
+    @GetMapping("/trade/item/threerow")
+    public ResponseEntity<?> searchOneRowPerItem(@RequestParam String startDate,
+                                                 @RequestParam String endDate) throws JsonProcessingException {
+        List<Map<String, Object>> list = itemGraphService.findThreeRowPerItem(startDate,endDate);
+        startDate = Change(startDate);
+        endDate = Change(endDate);
+        return ResponseEntity.ok(new ItemRow1ResponseDTO(list,startDate,endDate));
+    }
+
+    @GetMapping("/trade/item/zerorow")
+    public ResponseEntity<?> searchZeroRowPerITem(@RequestParam String item,
+                                                  @RequestParam String startDate,
+                                                  @RequestParam String endDate) throws JsonProcessingException {
+        Map<String, Object> list = itemGraphService.findZeroRowPerItem(item,startDate,endDate);
+        startDate = Change(startDate);
+        endDate = Change(endDate);
         return ResponseEntity.ok(list);
     }
 
--- back/ssafycnt-trade-service/src/main/java/com/ssafy/ssafycnttradeservice/dto/response/stat/StatRow3ResponseDTO.java
+++ back/ssafycnt-trade-service/src/main/java/com/ssafy/ssafycnttradeservice/dto/response/stat/StatRow3ResponseDTO.java
@@ -1,4 +1,4 @@
-package com.ssafy.ssafycnttradeservice.dto.response;
+package com.ssafy.ssafycnttradeservice.dto.response.stat;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -10,12 +10,12 @@ import java.util.Map;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class Row4ResponseDTO {
+public class StatRow3ResponseDTO {
     private String period;
     private Map<String, Object> exportDetail;
     private Map<String, Object> importDetail;
 
-    public Row4ResponseDTO(List<Map<String, Object>> list, String startDate, String endDate) {
+    public StatRow3ResponseDTO(List<Map<String, Object>> list, String startDate, String endDate) {
         this.period = startDate + " ~ " + endDate;
         exportDetail = list.get(0);
         importDetail = list.get(1);
