ad71dad2b209954680d4b5e47115292f26d96033
김진희 <molihua321.dp@gmail.com>
2023-04-03 17:04:24 +0900

    Feat: 회원가입, 404페이지 생성

--- front/src/components/pages/Login/index.jsx
+++ front/src/components/pages/Login/index.jsx
@@ -1,42 +1,44 @@
 import React, { useState } from "react";
-
+import { useParams, useNavigate } from "react-router-dom";
+import logo from "./../../../assets/logo.svg";
 import "./login.scss"
 
 function Login(props) {
+  const navigate = useNavigate();
 
   return (
     <>
+    <div className="z-30 sticky top-0">
+      <nav className="flex justify-between  sticky top-0 bg-slate-200 content-center font-mun">
+      <button
+          onClick={() => {
+            navigate("/nation/ALL/202203-202302");
+          }}
+        >
+          <img src={logo} className="w-32 h-32 ml-10" />
+        </button>
+      </nav>
+    </div>
     <div class="loginForm">
 
       <form>
   
         <div class="segment">
-          <h1>싸피물산</h1>
+          <h1>ONE & ONLY NO IN JUNG.</h1>
         </div>
         
         <label>
-          <input type="email" placeholder="이메일"/>
+          <input type="email" placeholder=" 이메일"/>
         </label>
         <label>
-          <input type="password" placeholder="비밀번호"/>
+          <input type="password" placeholder=" 비밀번호"/>
         </label>
-        <button class="red" type="button"><i class="icon ion-md-lock"></i>로그인</button>
-        <button class="red signup" type="button"><i class="icon ion-md-lock"></i>회원 가입</button>
-
-        
-        {/* <div class="segment">
-          <button class="unit" type="button"><i class="icon ion-md-arrow-back"></i></button>
-          <button class="unit" type="button"><i class="icon ion-md-bookmark"></i></button>
-          <button class="unit" type="button"><i class="icon ion-md-settings"></i></button>
-        </div>
-        
-        <div class="input-group">
-          <label>
-            <input type="text" placeholder="Email Address"/>
-          </label>
-          <button class="unit" type="button"><i class="icon ion-md-search"></i></button>
-        </div> */}
-  
+        <button onClick={() => {
+            navigate("/err404");
+          }} class="red" type="button"><i class="icon ion-md-lock"></i>로그인</button>
+        <button onClick={() => {
+            navigate("/signup");
+          }} class="red signup" type="button"><i class="icon ion-md-lock"></i>회원 가입</button>
       </form>
     </div>
     </>
