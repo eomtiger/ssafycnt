03cfc5d85f57cfdc565826916f279cd51a4184c3
엄희원 <eomtiger@gmail.com>
2023-04-04 16:13:28 +0900

    push force

--- front/src/components/molecules/navBar/Excel/index.jsx
+++ front/src/components/molecules/navBar/Excel/index.jsx
@@ -1,11 +1,14 @@
-import React, {useState, useEffect} from 'react';
-import * as XLSX from 'xlsx';
+import React, { useState, useEffect } from "react";
+import * as XLSX from "xlsx";
 import excel from "../../../../assets/excel.svg";
 // import tradeData from "../../../../assets/trade_data.json";
 // import tradeData from "./trade_data.json";
 import tradeData1 from "./exelData1.json";
 import tradeData2 from "./exelData2.json";
 import tradeData3 from "./exelData3.json";
+
+import { useRecoilValue } from "recoil";
+import { excelState3 } from "../../../../states/Excel";
 
 function Excel() {
   const [data1, setData1] = useState([]);
@@ -17,14 +20,35 @@ function Excel() {
       ["nation", data1.nationName],
       ["period", data1.period],
       [],
-      ['expdlrSum', 'impdlrSum', 'balpaymentsLr', 'expwgtSum', 'impwgtSum', 'balpaymentsWgt'],
-      [data1.expdlrSum, data1.impdlrSum, data1.balpaymentsLr, data1.expwgtSum, data1.impwgtSum, data1.balpaymentsWgt],  
+      [
+        "expdlrSum",
+        "impdlrSum",
+        "balpaymentsLr",
+        "expwgtSum",
+        "impwgtSum",
+        "balpaymentsWgt",
+      ],
+      [
+        data1.expdlrSum,
+        data1.impdlrSum,
+        data1.balpaymentsLr,
+        data1.expwgtSum,
+        data1.impwgtSum,
+        data1.balpaymentsWgt,
+      ],
     ];
-  
+
     // Add a blank row between the tables
     table.push([]);
-    table.push(['item', 'exppdlrSum', 'changeRate1', 'changeRate2', 'export1', 'export2']);
-  
+    table.push([
+      "item",
+      "exppdlrSum",
+      "changeRate1",
+      "changeRate2",
+      "export1",
+      "export2",
+    ]);
+
     // Add exportTop data to the table
     Object.keys(data2.exportTop).forEach((key) => {
       const topItem = data2.exportTop[key];
@@ -37,18 +61,37 @@ function Excel() {
         topItem.exportChange["2023.02"],
       ]);
     });
-  
+
     return table;
   }
-  
+
+  const newData = useRecoilValue(excelState3);
+
   function convertData3ToTable(data) {
     const table = [
-      ['period', data.period],
+      ["period", data.period],
       [],
-      ['Export Detail', 'ranking', 'nationName', 'expdlrSum', 'expdlrRatio', 'expwgtSum', 'expwgtRatio', 'hsCode','',
-       'Import Detail', 'ranking', 'nationName', 'expdlrSum', 'expdlrRatio', 'expwgtSum', 'expwgtRatio', 'hsCode',],
+      [
+        "Export Detail",
+        "ranking",
+        "nationName",
+        "expdlrSum",
+        "expdlrRatio",
+        "expwgtSum",
+        "expwgtRatio",
+        "hsCode",
+        "",
+        "Import Detail",
+        "ranking",
+        "nationName",
+        "expdlrSum",
+        "expdlrRatio",
+        "expwgtSum",
+        "expwgtRatio",
+        "hsCode",
+      ],
     ];
-  
+
     // Add exportDetail data to the table
     const exportDetailKeys = Object.keys(data.exportDetail);
     for (let i = 0; i < exportDetailKeys.length; i++) {
@@ -59,7 +102,7 @@ function Excel() {
       if (!table[i + 3]) {
         table[i + 3] = [];
       }
-      // add push 
+      // add push
       table[i + 3].push(
         key,
         detail.ranking,
@@ -68,15 +111,15 @@ function Excel() {
         detail.expdlrRatio,
         detail.expwgtSum,
         detail.expwgtRatio,
-        detail.hsCode,
+        detail.hsCode
       );
     }
 
     // Add a blank column between the tables
     for (let i = 3; i < table.length; i++) {
-      table[i].push('');
+      table[i].push("");
     }
-  
+
     // Add a blank column between the tables
     const importDetailKeys = Object.keys(data.importDetail);
     for (let i = 0; i < importDetailKeys.length; i++) {
@@ -94,13 +137,12 @@ function Excel() {
         detail.impdlrRatio,
         detail.impwgtSum,
         detail.impwgtRatio,
-        detail.hsCode,
+        detail.hsCode
       );
     }
-  
+
     return table;
   }
-  
 
   useEffect(() => {
     setData1(tradeData1);
@@ -110,17 +152,17 @@ function Excel() {
 
   const exportToExcel = () => {
     const ws1 = XLSX.utils.aoa_to_sheet(convertData2ToTable(data1, data2));
-    const ws2 = XLSX.utils.aoa_to_sheet(convertData3ToTable(data3));
+    const ws2 = XLSX.utils.aoa_to_sheet(convertData3ToTable(newData));
 
     const wb = XLSX.utils.book_new();
-    XLSX.utils.book_append_sheet(wb, ws1, '수출입 비중 & top 5');
-    XLSX.utils.book_append_sheet(wb, ws2, '수출입 상세');
-    XLSX.writeFile(wb, '보고서.xlsx');
+    XLSX.utils.book_append_sheet(wb, ws1, "수출입 비중 & top 5");
+    XLSX.utils.book_append_sheet(wb, ws2, "수출입 상세");
+    XLSX.writeFile(wb, "보고서.xlsx");
   };
 
   return (
-    <button onClick={exportToExcel} >
-        <img src={excel} className="w-10 h-10" />
+    <button onClick={exportToExcel}>
+      <img src={excel} className="w-10 h-10" />
     </button>
   );
 }
