ddf4cf5e30ac6dfaedf8a3ff07ea3943181f2057
엄희원 <eom9300@hanmail.net>
2023-04-04 16:13:28 +0900

    push force

--- front/src/components/organism/DataSecond/index.jsx
+++ front/src/components/organism/DataSecond/index.jsx
@@ -1,19 +1,18 @@
-import BarChartExport from './../../molecules/dataSecond/ExportTop5/index'
-import BarChartImport from './../../molecules/dataSecond/ImportTop5/index'
-import LineChartTrend from './../../molecules/dataSecond/LineGraph/index'
-import { useState, useEffect } from 'react'
+import BarChartExport from "./../../molecules/dataSecond/ExportTop5/index";
+import BarChartImport from "./../../molecules/dataSecond/ImportTop5/index";
+import LineChartTrend from "./../../molecules/dataSecond/LineGraph/index";
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 
-
 // function DataSecond() {
 //   const params = useParams();
-  
+
 //   let firstExportData
 //   let Top5Data
-  
+
 //   // const [currentState, chagneState] = useState([0, 0, '모든 품목', '수출', firstExportData]) // [Export, Import] clicked???
-  
+
 //   useEffect(() => {
 //     axios
 //       .get(
@@ -29,7 +28,7 @@ import axios from "axios";
 //         console.log(Top5Data)
 //       })
 //     }, [params]);
-    
+
 //     const [currentState, chagneState] = useState([0, 0, '모든 품목', '수출', firstExportData]) // [Export, Import] clicked???
 //     console.log(currentState)
 
@@ -54,21 +53,48 @@ import axios from "axios";
 
 function DataSecond() {
   const params = useParams();
-  const [currentState, changeState] = useState([0, 0, '모든 품목', '수출', [], {}, '전세계', {}]); // initialize the state with an empty array
+  const [currentState, changeState] = useState([
+    0,
+    0,
+    "모든 품목",
+    "수출",
+    [],
+    {},
+    "전세계",
+    {},
+  ]); // initialize the state with an empty array
   // http://ssafycnt.site:8000/ssafycnt-trade-service/api/trade/tworow?statCd=US&startDate=202201&endDate=202203
   useEffect(() => {
     axios
       .get(
         "https://ssafycnt.site:8000/ssafycnt-trade-service/api/trade/tworow?" +
-          "statCd=" + params.nationCode + "&" +
-          "startDate=" + params.duration.substring(0,6) + "&" +
-          "endDate=" + params.duration.substring(7,13)
+          "statCd=" +
+          params.nationCode +
+          "&" +
+          "startDate=" +
+          params.duration.substring(0, 6) +
+          "&" +
+          "endDate=" +
+          params.duration.substring(7, 13)
       )
       .then((response) => {
         const firstExportData = response.data.expdlrChange;
-        const Top5Data = { '수출': response.data.exportTop, '수입': response.data.importTop };
-        const nation = response.data.nationName
-        changeState([0, 0, '모든 품목', '수출', firstExportData, Top5Data, nation, firstExportData]);
+        const Top5Data = {
+          수출: response.data.exportTop,
+          수입: response.data.importTop,
+        };
+        const nation = response.data.nationName;
+        changeState([
+          0,
+          0,
+          "모든 품목",
+          "수출",
+          firstExportData,
+          Top5Data,
+          nation,
+          firstExportData,
+        ]);
+        console.log(response.data);
       })
       .catch((error) => {
         console.log(error);
@@ -76,17 +102,41 @@ function DataSecond() {
   }, [params]);
 
   const onChangeExportClick = (item) => {
-    changeState([1, 0, item, '수출', currentState[4], currentState[5], currentState[6], currentState[5]['수출'][item]['exportChange']]);
-  }
+    changeState([
+      1,
+      0,
+      item,
+      "수출",
+      currentState[4],
+      currentState[5],
+      currentState[6],
+      currentState[5]["수출"][item]["exportChange"],
+    ]);
+  };
   const onChangeImportClick = (item) => {
-    changeState([0, 1, item, '수입', currentState[4], currentState[5], currentState[6], currentState[5]['수입'][item]['importChange']]);
+    changeState([
+      0,
+      1,
+      item,
+      "수입",
+      currentState[4],
+      currentState[5],
+      currentState[6],
+      currentState[5]["수입"][item]["importChange"],
+    ]);
   };
 
   return (
     <div className="flex justify-center space-x-5 mt-7">
       <LineChartTrend anyItem={currentState} />
-      <BarChartExport alreadyClicked={currentState} onSaveClickOrNot={onChangeExportClick} />
-      <BarChartImport alreadyClicked={currentState} onSaveClickOrNot={onChangeImportClick} />
+      <BarChartExport
+        alreadyClicked={currentState}
+        onSaveClickOrNot={onChangeExportClick}
+      />
+      <BarChartImport
+        alreadyClicked={currentState}
+        onSaveClickOrNot={onChangeImportClick}
+      />
     </div>
   );
 }
