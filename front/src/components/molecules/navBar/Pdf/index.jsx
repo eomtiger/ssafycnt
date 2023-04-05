1174cb0e757ea84c6787fb2b03ddbe8f2bf5ac52
powerjjh95 <powerjjh95@gmail.com>
2023-04-05 09:53:31 +0900

    Feat : Nation Pdf Create

--- front/src/components/molecules/navBar/Pdf/index.jsx
+++ front/src/components/molecules/navBar/Pdf/index.jsx
@@ -1,11 +1,25 @@
 import React from "react";
 import jsPDF from "jspdf";
-import html2canvas from "html2canvas";
 import pdf from "../../../../assets/pdf.svg";
-import Code from "../../../../assets/Code.json";
 import { useParams } from "react-router-dom";
+import { useRecoilValue } from "recoil";
+import {
+  data1ImgAtom,
+  data2ImgAtom,
+  data3ImgAtom,
+  textMiningImgAtom,
+} from "../../../../states/recoilPdfState";
 
 function Pdf() {
+  const data1Img = useRecoilValue(data1ImgAtom);
+  const data2Img = useRecoilValue(data2ImgAtom);
+  const data3Img = useRecoilValue(data3ImgAtom);
+  const textMiningImg = useRecoilValue(textMiningImgAtom);
+  // console.log(data1Img);
+  // console.log(data2Img);
+  // console.log(data3Img);
+  // console.log(textMiningImg)
+
   const params = useParams();
 
   const pdfText = `Nation : ${params.nationCode}, Duration : ${params.duration}`;
@@ -13,6 +27,10 @@ function Pdf() {
   const downloadPdf = () => {
     const pdf = new jsPDF();
     pdf.text(pdfText, 10, 10);
+    pdf.addImage(data1Img, "JPEG", 0, 20);
+    pdf.addImage(data2Img, "JPEG", 0, 50);
+    pdf.addImage(data3Img, "JPEG", 0, 80);
+    pdf.addImage(textMiningImg, "JPEG", 0, 100);
     pdf.save(`Report_${params.nationCode}_${params.duration}`);
   };
 
