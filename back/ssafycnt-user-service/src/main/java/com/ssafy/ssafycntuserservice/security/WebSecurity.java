2104d8d359f80aebd1975afa9e6f60b5132690ce
김이삭 <isakggong@gmail.com>
2023-04-05 10:45:13 +0900

    Merge: update user service

--- back/ssafycnt-user-service/src/main/java/com/ssafy/ssafycntuserservice/security/WebSecurity.java
+++ back/ssafycnt-user-service/src/main/java/com/ssafy/ssafycntuserservice/security/WebSecurity.java
@@ -1,6 +1,7 @@
 package com.ssafy.ssafycntuserservice.security;
 
 import com.ssafy.ssafycntuserservice.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
@@ -9,7 +10,11 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.servlet.Filter;
 
@@ -28,15 +33,36 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();
-        http.authorizeRequests().antMatchers("/users/**").permitAll();
+        // Cross origin resource sharing
+        http.cors().and()
+                // starts authroizing configurations
+                .authorizeRequests()
+                // ignore the "/" and "/index.html"
+                .antMatchers("/resources/**", "/error", "/service/**").permitAll()
+                // authenticate all remaining URLS
+                .anyRequest().fullyAuthenticated()
+                .and()
+                .logout().permitAll()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/service/logout", "POST"))
+                // login form
+                .and()
+                .formLogin().loginPage("/service/login").and()
+                // enable basic header authentication
+                .httpBasic().and()
+                // Cross Site Request Forgery
+                .csrf().disable();
+
+
+        //        http.csrf().disable();
+//        http.authorizeRequests().antMatchers("/users/**").permitAll();
 //        http.authorizeRequests().antMatchers("/**")
 //                .hasIpAddress("192.168.31.195") // IP 변경 (Wifi)
 //                .and()
 //                .addFilter(getAuthenticationFilter());
-//
-        http.headers().frameOptions().disable();
+//        http.headers().frameOptions().disable();
     }
+
+
 
     private AuthenticationFilter getAuthenticationFilter() throws Exception{
         AuthenticationFilter authenticationFilter = new AuthenticationFilter();
@@ -49,5 +75,16 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userService).passwordEncoder(bCryptPasswordEncoder);
     }
+
+
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
+    }
+
 }
 
