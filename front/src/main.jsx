1a280d95a8b4749514d03465719f97cb035f54e2
엄희원 <eomtiger@gmail.com>
2023-04-06 12:57:53 +0900

    fix: app, main 주석

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -6,6 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 import { RecoilRoot } from "recoil";
 
 ReactDOM.render(
+  //recoil 전역 상태 관리 및 라우터 path 적용
   <RecoilRoot>
     <BrowserRouter>
       <App />
