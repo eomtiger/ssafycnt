aa72ac2b4c78e35c0e130531a6850e075b23ee99
kwanseob <foreverdunk@naver.com>
2023-04-06 16:45:11 +0900

    Feat: ester egg

--- front/src/components/organism/Footer/index.jsx
+++ front/src/components/organism/Footer/index.jsx
@@ -2,7 +2,7 @@ function Footer() {
     return (
         <div className="font-mun mb-16 mt-10">
             <p>회사소개&nbsp;&nbsp;&nbsp;&nbsp;인재채용&nbsp;&nbsp;&nbsp;&nbsp;제휴제안&nbsp;&nbsp;&nbsp;&nbsp;이용약관&nbsp;&nbsp;&nbsp;&nbsp;개인정보처리방침&nbsp;&nbsp;정책고객센터&nbsp;&nbsp;&nbsp;&nbsp;  
-                <a href="https://www.notion.so/0a778518a34940709676e859dce730f4?v=7de882ea6df24d7c82f04f8150b32919&pvs=4">ⓒ Noinjeong Corp.</a>
+                <a target="_blank" href="https://www.notion.so/0a778518a34940709676e859dce730f4?v=7de882ea6df24d7c82f04f8150b32919&pvs=4">ⓒ Noinjeong Corp.</a>
             </p>
         </div>
     );
